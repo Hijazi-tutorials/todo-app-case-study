84acb9f270d82698c3c992297d1cd04d6ad6b355
fadi.hijazi <fadihijazi2018@outlook.com>
2022-10-21 15:14:23 +0300

    add new family of font to app & styled as `.font-source-code-pro` class

--- template.php
+++ template.php
@@ -14,12 +14,20 @@
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@200;400;700&display=swap" rel="stylesheet">
     <title>
         <?php echo (isset($title) ? $title : 'index') ?>
     </title>
+    <style>
+        .font-source-code-pro {
+            font-family: 'Source Code Pro', monospace;
+        }
+    </style>
 </head>
 <body>
-<div id="main" class="h-screen bg-gray-200 p-8">
+<div id="main" class="min-h-screen bg-gray-200 p-8">
     <!-- content -->
     <?php
     echo(isset($content) ? $content : '<h1>The view should override `$content` value</h1>');
