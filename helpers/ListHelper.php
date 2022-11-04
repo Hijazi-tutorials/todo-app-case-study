244995eee64fbcfd898efcedd4d5dcacb579bff5
fadi.hijazi <fadihijazi2018@outlook.com>
2022-11-04 17:35:50 +0200

    enhance `ListHelper`.(`getValue`, `getConfigurationValue`) logic.

--- helpers/ListHelper.php
+++ helpers/ListHelper.php
@@ -5,16 +5,23 @@ class ListHelper
     /**
      * @throws Exception if key isn't exist & default isn't provided
      */
-    public static function getValue(string $key, array $list, string $default = null, string $exception_msg = null): string
+    public static function getValue(string $key, array $list, string $default = null): string
     {
         if (!in_array($key, array_keys($list)) && !$default) {
-            throw new Exception($exception_msg ?? "key of list isn't exist, passing the `\$default` param to handle this exception.");
+            throw new Exception("key of list isn't exist, passing the `\$default` param to handle this exception.");
         }
 
         return $list[$key] ?? $default;
     }
 
+    /**
+     * @throws Exception if $key isn't in configuration
+     */
     public static function getConfigurationValue(string $key, array $configurations) {
-        return self::getValue($key, $configurations, null, "[OVERRIDE]: Add `$key` config value to your local configuration.");
+        try {
+            return self::getValue($key, $configurations);
+        } catch (Exception $e) {
+            throw new Exception("[OVERRIDE]: Add `$key` config value to your local configuration.");
+        }
     }
 }
