558c0f468f1ba0fbb737b8dcb89e2a453e025d34
fadi.hijazi <fadihijazi2018@outlook.com>
2022-11-04 17:04:20 +0200

    new `ListHelper`with new `getValue`, & `getConfigurationValue` methods to extract values from associated array (list) within handle default, & exception cases.

---
+++ helpers/ListHelper.php
@@ -0,0 +1,20 @@
+<?php
+
+class ListHelper
+{
+    /**
+     * @throws Exception if key isn't exist & default isn't provided
+     */
+    public static function getValue(string $key, array $list, string $default = null, string $exception_msg = null): string
+    {
+        if (!in_array($key, array_keys($list)) && !$default) {
+            throw new Exception($exception_msg ?? "key of list isn't exist, passing the `\$default` param to handle this exception.");
+        }
+
+        return $list[$key] ?? $default;
+    }
+
+    public static function getConfigurationValue(string $key, array $configrations) {
+        return self::getValue($key, $configrations, null, "[OVERRIDE]: Add `$key` config value to your local configuration.");
+    }
+}
