d9615b0ab9ed17e850ce349f0ea0ccf3522e1857
fadi.hijazi <fadihijazi2018@outlook.com>
2022-11-07 22:34:14 +0200

    assign item as completed using database instead of session

--- actions/assign_item_as_completed.php
+++ actions/assign_item_as_completed.php
@@ -1,30 +1,37 @@
 <?php
 require_once '../utils/buffer_session_init.php';
-require_once '../helpers/UniqueGeneratorHelper.php';
-require_once '../helpers/DateHelper.php';
 require_once '../helpers/RedirectHelper.php';
+require_once '../helpers/DatabaseHelper.php';
 
 if ($_SERVER['REQUEST_METHOD'] != 'POST') {
     throw new Exception("Can't assign todo-item as completed without POST request.");
 }
-if (! key_exists('item_id', $_POST)) {
+if (!key_exists('item_id', $_POST)) {
     throw new Exception("Can't create todo item without title.");
 }
 
 $item_id = $_POST['item_id'];
-$item = $todoItems[$item_id];
 
-// this like move item from to.do array (by remove item from the list) to completed array (by add item to the list)
-unset($_SESSION['items']['todo'][$item_id]);
-unset($todoItems[$item_id]);
+$item =
+    DatabaseHelper::fetchItem(
+        "SELECT * FROM todo_items WHERE id = $item_id",
+        [...DatabaseHelper::$common_fields, 'created_at']
+    );
 
-$_SESSION['items']['completed'][$item_id] = [
-    'id' => $item_id,
-    'title' => $item['title'],
-    'description' => $item['description'],
-    'created_at' => $item['created_at'],
-    'completed_at' => DateHelper::humanDateFormat()
-];
+if (! $item) {
+    throw new Exception("item #$item_id not exist.");
+}
+
+// delete todo item from `todo_items` table
+DatabaseHelper::mysqliConnection()->query("DELETE FROM `todo_items` WHERE id = $item_id");
+
+// add the fetched fields to `completed_items` table
+DatabaseHelper::mysqliConnection()->query(
+    sprintf(
+        "INSERT INTO `completed_items` (`%s`, `%s`) VALUES ('%s', '%s')",
+        "title", "description", $item['title'], $item['description']
+    )
+);
 
 $_SESSION['redirect_message'] =
     sprintf(
