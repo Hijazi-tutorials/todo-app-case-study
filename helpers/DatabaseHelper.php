a257780c61f932df4dd447d42cfcbd77caddf0f3
fadi.hijazi <fadihijazi2018@outlook.com>
2022-11-09 19:59:02 +0200

    update DatabaseHelper to support CRD resource, by new `fetchResource` instead of 'fetchItem', createResource, and deleteResource.

--- helpers/DatabaseHelper.php
+++ helpers/DatabaseHelper.php
@@ -67,9 +67,63 @@ class DatabaseHelper
         return $items;
     }
 
-    public static function fetchItem($query, $field): array
+    public static function fetchResource($tableName, $itemId, $fields, $throwExceptionIfNotExist=false): array
     {
-        return self::fetchItems($query, $field)[0] ?? [];
+        $item = self::fetchItems(
+            "SELECT * FROM `$tableName` WHERE `id`=$itemId",
+            $fields
+        );
+
+        if(! $item & $throwExceptionIfNotExist) {
+            throw new Exception("resource #$itemId does not exist in #$tableName");
+        } elseif (! $item) {
+            return [];
+        } else {
+            return $item[0];
+        }
+    }
+
+    public static function createResource($tableName, $fields, $values)
+    {
+        if (! is_array($fields)) {
+            throw new Exception("createResource accept fields as array.");
+        } elseif (! is_array($values)) {
+            throw new Exception("createResource accept values as array.");
+        }
+
+        // loop through $values array, & wrap each element with single-quotation (')
+        $wrappedValues =
+            array_map(function ($value) {
+                return "'$value'";
+            }, $values);
+
+        $insertQuery =
+            sprintf(
+                "INSERT INTO $tableName (%s) VALUES (%s)",
+                join(", ", $fields),
+                join(", ", $wrappedValues)
+            );
+
+        if(! self::$mysqli->query($insertQuery))
+        {
+            throw new Exception(
+                "something wrong with this query: '$insertQuery'." .
+                " [HINT] make sure the table name, fields, and values all are correct."
+            );
+        }
+    }
+
+    public static function deleteResource($tableName, $itemId)
+    {
+        $deleteResourceQuery = "DELETE FROM $tableName WHERE id = $itemId";
+
+        if(! self::$mysqli->query($deleteResourceQuery))
+        {
+            throw new Exception(
+                "something wrong with this query: '$deleteResourceQuery'." .
+                " [HINT] make sure the table name & item id both are correct."
+            );
+        }
     }
 }
 
